fccb00ac3f25526e5a3cd905ab77c0130b99a0df
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2023-12-23 19:01:28 +0200

    added to utils classes

---
+++ src/main/java/org/example/util/Constants.java
@@ -0,0 +1,9 @@
+package org.example.util;
+
+import java.sql.Connection;
+
+public class Constants {
+    protected static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/java-dev-hw-7";
+    protected static final String DB_USER = "postgres";
+    protected static final String DB_PASS = "sempai";
+}
